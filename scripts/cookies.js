d82211ef5192244e2810b05b18e36dff045c981a
Paul <pmazak2002@yahoo.com>
2011-03-27 21:51:13 -0400

    restored save feature by bringing back js files from old site

---
+++ scripts/cookies.js
@@ -0,0 +1,40 @@
+//Global Variables
+var COOKIE_DELIMITER = "|***|";
+var COUNTERCOOKIE = "counted";
+var BLOGCOOKIE = "evo_style";
+
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+
+function readCookieNames() {
+	var cookieNames = new Array();
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf('=') != -1) cookieNames.push(c.substring(0, c.indexOf('=')));
+	}
+	return cookieNames;
+}
